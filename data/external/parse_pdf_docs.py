e78bb0339d039d0c338466e90fe59cf003b4e62b
ausec-tech-team <03shruthi.b@gmail.com>
2024-09-17 17:01:35 +0530

    Scraping PDF data - 1
    Scraping data from 28 PDFs from multiple sources

--- data/external/parse_pdf_docs.py
+++ data/external/parse_pdf_docs.py
@@ -1,50 +1,50 @@
 import json
-import fitz
+import pdfplumber
 
-def open_doc(file_name:str):
-    return fitz.open(file_name)
-class Extract_ELD:
-    file_name:str
-    output_file_name:str
-    temp_data_store:dict
-    doc = None
-    def __init__(self,file_name:str):
-        self.file_name = file_name
-        self.doc = open_doc(self.file_name)
-        self.temp_data_store = {
-            'pdf_name' : self.file_name.split('/')[-1],
-            'total_pages' : self.doc.page_count,
-            'content' : {}
-        }
-    
-    def __iter(self)->None:
-        page:None
-        for page_num in range(self.temp_data_store['total_pages']):
-            page = self.doc.load_page(page_num)
-            page_text = page.get_text('text')
-            
-            self.temp_data_store['content'][f'page_{page_num+1}'] = self.temp_data_store['content'].get(f'page_{page_num+1}', "") + page_text.strip()
-    def __close(self)->None:
-        self.doc.close()
-    
-    def save_to_json(self,output_file_name:str)->None:
-        self.__iter()
-        self.__close()
-        self.output_file_name = output_file_name
-        with open(self.output_file_name, "a+", encoding='utf-8') as file:
-            json.dump(self.temp_data_store, file,ensure_ascii=False, indent=4)
-    
-    def reopen_doc(self,file_name:str)->None:
-        self.file_name = file_name
-        self.doc = open_doc(self.file_name)
 
-class Extract_MLD:
-    pass
+
+class Extract_EDL:
+    file_path:str=""
+    output_file_path:str=""
+    section_headers:list[str] = ['Abstract','keywords',
+        'Introduction', 'Background', 'Summary','Objectives','Discussion', 'Material and Methods',
+        'Methods','Methodology', 'Observations', 'Clinical', 'Experiments', 'Effects','Results','conclusion',
+        'References','Acknowledgement'
+    ]
+    data:dict[str]={sect:[] for sect in section_headers}
+    
+    def __init__(self,**kwargs):
+        #self.file_path = kwargs['file_path']
+        self.output_file_path = kwargs['output_file_path']
+    
+    def __extract_data(self,**kwargs):
+        section_title = kwargs['header']
+        self.file_path = kwargs['input_file_path']
+        with pdfplumber.open(self.file_path) as pdf:
+            for page in pdf.pages:
+                text = page.extract_text()
+                lines = text.split('\n')
+                in_section = False
+                for line in lines:
+                    if line.strip().lower() == section_title.lower():
+                        in_section = True
+                    elif in_section and line.strip() == '':
+                        break
+                    elif in_section:
+                        self.data[section_title].append(line.strip())
+    def __format(self):
+        with open(self.output_file_path,'a+',encoding='utf-8') as json_file:
+            json.dump(self.data, json_file,indent=4,ensure_ascii=False)
+    
+    def extract_and_format(self,**kwargs):
+        for header in self.section_headers:
+            self.__extract_data(input_file_path=kwargs['input_file_path'],header=header)
+        self.__format()
     
 def main(**kwargs):
-    et = Extract_ELD(file_name=kwargs['file_name'])
-    et.save_to_json(output_file_name=kwargs['output_file_name'])
+    et = Extract_EDL(output_file_path=kwargs['output_file_path'])
+    for i in range(kwargs['start'],kwargs['end']+1):
+        et.extract_and_format(input_file_path=rf'../../data/doc/pdf-{i}.pdf')
     
 if __name__ == '__main__':
-    #for i in range(3,4):
-    main(file_name=rf'../../data/doc/pdf-{3}.pdf', output_file_name=rf'../../data/raw/pdf.json')
+    main(output_file_path=rf'../../data/raw/pdf.json',start=1,end=28)
--- data/external/parse_web.py
+++ data/external/parse_web.py
@@ -8,5 +8,15 @@ url_list:list[str] = [
     "https://www.planetayurveda.com/herbs-a-to-z/",
     "https://www.wbhealth.gov.in/WBSMPB/medicinal_plants_available.php",
     "https://www.wbhealth.gov.in/WBSMPB/29_Very_prioritized_prohobited_species.php",
-    
+    "https://journals.lww.com/aayu/fulltext/2018/39040/in_vitro_anti_inflammatory_activity_of_ficus.8.aspx#",
+    "https://journals.lww.com/aayu/fulltext/2024/45020/dip_eyes_in_water_technique__akshi_nimanjjana__.7.aspx",
+    "https://journals.lww.com/aayu/fulltext/2024/45020/evaluation_of_jati__jasminum_grandiflorum_l__.4.aspx",
+    "https://pubmed.ncbi.nlm.nih.gov/25379463/",
+    "https://pubmed.ncbi.nlm.nih.gov/38685809/",
+    "https://pubmed.ncbi.nlm.nih.gov/37254529/",
+    "https://pubmed.ncbi.nlm.nih.gov/33033566/",
+    "https://pubmed.ncbi.nlm.nih.gov/27011715/",
+    "https://pubmed.ncbi.nlm.nih.gov/35929620/",
+    "https://pubmed.ncbi.nlm.nih.gov/39138188/",
 ]
+
